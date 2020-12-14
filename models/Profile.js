d5c240243fdd558882755178823b0e90f71e1e73
David <david.ajayi.anu@gmail.com>
2020-12-14 13:19:36 +0100

    profile routes setup

--- models/Profile.js
+++ models/Profile.js
@@ -18,7 +18,7 @@ const ProfileSchema = new mongoose.Schema({
         type: String
     },
     skills: {
-        type: [string],
+        type: [String],
         require: true
     },
     bio: {
@@ -26,5 +26,88 @@ const ProfileSchema = new mongoose.Schema({
     },
     githubusername: {
         type: String
+    },
+    experience: [
+        {
+            title: {
+                type: String,
+                required: true
+            },
+            company: {
+                type: String,
+                required: true
+            },
+            location: {
+                type: String
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: Date
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            description: {
+                type: String
+            }
+        }
+    ],
+    education: [
+        {
+            school: {
+                type: String,
+                required: true
+            },
+            degree: {
+                type: String,
+                required: true
+            },
+            fieldofstudy: {
+                type: String
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: Date
+            },
+            current: {
+                type: Boolean,
+                default: false
+            },
+            description: {
+                type: String
+            }
+        }
+    ],
+    socials: [
+        {
+            youtube: {
+                type: String
+            },
+            twitter: {
+                type: String
+            },
+            facebook: {
+                type: String
+            },
+            linkedin: {
+                type: String
+            },
+            instagram: {
+                type: String
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now()
     }
 })
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema)
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,14 +1,46 @@
 const express = require('express')
-
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
+const auth = require('../../middleware/auth')
+const { check, validationResult } = require('express-validator')
 const router = express.Router()
 
-// @route POST api/profile
-// @desc Profile routes
-// @access Public
+// @route GET api/profile/me
+// @desc Get current user profile
+// @access Private
 
-router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('Profile route')
+router.get('/me', auth, async (req, res) => {
+    try {
+        const id = req.user.id
+        const profile = await Profile.findOne({ user: id }).populate('user', [
+            'name', 'avatar'
+        ])
+        
+        if(!profile) {
+            return res.status(400).json({ msg: 'There is no profile for this user' })
+        }
+
+        res.json(profile)
+    }catch(err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+// @route GET api/profile           
+// @desc Create or update users profile
+// @access Private
+
+router.post('/', [
+    auth, 
+    check('status', 'Status is required').not().isEmpty(),
+    check('skills', 'Skills is required').not().isEmpty()
+],  
+async (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() })
+    }  
 })
 
 module.exports = router
